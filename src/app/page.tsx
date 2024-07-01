545ac7e0eb76fbc6c70373d7a23918f459a04ded
Karoline <karoline_mj@hotmail.com>
2024-07-01 19:39:08 +0200

    get data from dummyjson

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,31 @@
+'use client'
+import { getURL } from '@/app/utils'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 export default function Home() {
-  return <p>Hello Developers!</p>
+  const [productList, setProductList] = useState([])
+  useEffect(() => {
+    const getAllProductList = async () => {
+      const response = await axios.get(getURL())
+      setProductList(response.data.products)
+    }
+    getAllProductList()
+  }, [])
+
+  return (
+    <div>
+      {productList.length > 0 &&
+        productList.map(
+          (
+            product: {
+              title: string
+            },
+            index,
+          ) => {
+            return <p key={index}>{product.title}</p>
+          },
+        )}
+    </div>
+  )
 }
---
+++ src/app/utils.ts
@@ -0,0 +1,7 @@
+export function getURL() {
+  let url = 'https://dummyjson.com/products'
+
+  // Add paramenters to url before returning for specific product lists
+
+  return url
+}
